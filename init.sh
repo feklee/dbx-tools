a5288e0f0e3f28d23d01f310f60d6dc740cde783
Felix E. Klee <felix.klee@inka.de>
2018-07-15 13:38:23 +0200

    Improve command line parsing

--- init.sh
+++ init.sh
@@ -1,23 +1,54 @@
 set -e
 
-function dbx-print-usage {
-    echo "Usage: `basename $0` $1"
-    test -n "$2" && echo "
-$2"
-}
-
 function dbx-test-absolute {
     [[ "${1:0:1}" == / || "${1:0:2}" == ~[/a-z] ]]
 }
 
 function dbx-exit-on-error {
-    echo "$1" 2>&1
+    echo "$1" >&2
     exit 1
 }
 
-function dbx-exit-on-usage {
-    dbx-print-usage "$1" "$2"
-    exit 1
+function dbx-exit-on-help {
+    EXIT_STATUS=${1-0}
+
+    echo -n "Usage: `basename $0`"
+
+    KEYS=${!OPTIONS[@]}
+    test -n "$KEYS" && echo -n " [-${KEYS// /] [-}]"
+    test -n "$NON_OPTION_ARGS" && echo -n " $NON_OPTION_ARGS"
+    echo
+
+    test -n "$DESCRIPTION" && echo && echo "$DESCRIPTION"
+
+    test -z "$KEYS" && exit $EXIT_STATUS
+    echo
+    for KEY in $KEYS; do
+	echo -e "  -$KEY\t${OPTIONS[$KEY]}"
+    done
+    exit $EXIT_STATUS
+}
+
+function dbx-exit-on-missing-args {
+    dbx-exit-on-help 1
+}
+
+function dbx-exit-on-invalid-option {
+    dbx-exit-on-error "Invalid option: -$1"
+}
+
+function parse_options {
+    KEYS=${!OPTIONS[@]}
+    OPTSTRING=":${KEYS// /}"
+    while getopts "$OPTSTRING" OPT; do
+	test $OPT = "?" && dbx-exit-on-invalid-option $OPTARG
+	test $OPT = "h" && dbx-exit-on-help
+	ENABLED_OPTIONS+=$OPT
+    done
+}
+
+function test_option_enabled {
+    [[ $ENABLED_OPTIONS = *$1* ]]
 }
 
 # TODO: Check installation of dbxcli
@@ -30,3 +61,6 @@ test -v DBX_HOME || dbx-exit-on-error "DBX_HOME undefined"
 ABSOLUTE_DBX_HOME=`realpath -e $DBX_HOME` || dbx-exit-on-error "\$DBX_HOME does not exist: $DBX_HOME"
 dbx-test-absolute "$DBX_HOME" || dbx-exit-on-error "\$DBX_HOME is not absolute"
 test -d "$DBX_HOME" || dbx-exit-on-error "\$DBX_HOME is not a directory: $DBX_HOME"
+
+declare -A OPTIONS
+OPTIONS[h]="explain usage"
